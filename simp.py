2b96fbd8dd2b02ca8d4adf111022ee729abbf74e
Wes Turner <50891+westurner@users.noreply.github.com>
2008-09-02 16:58:11 -0500

    refactoring to oo

--- simp.py
+++ simp.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 # simple app
-from rtm import createRTM
+from rtm import createRTM, RTM
 import logging
 from string import Template
 
@@ -31,12 +32,24 @@ def examp(rtm):
     return [(x.id, x.name,x.tags and x.tags.tag or []) for x in rtm.tasks.getList(filter='list:Inbox').tasks.list.taskseries]
 
 
-def rtm_connection():
-    # TODO: Proper subclass (ie no monkeypatch)
+class RMilk(RTM):
+    def __init__(self,apiKey=API_KEY,secret=API_SEC,token=API_TOK):
+        # TODO: Proper subclass (ie no monkeypatch)
+        super(RMilk,self).__init__(apiKey,secret,token)
+        
+        if 0 and token is None:
+            print 'No token found'
+            print 'Login here:', rtm.getAuthURL()
+            raw_input('Press enter after logging in at the link above')
+            token = rtm.getToken()
+            print 'Remember this token:', token
+            super(RMilk,self).__init__(self,apiKey,secret,token)
 
-    rtm = createRTM(API_KEY, API_SEC, API_TOK)
-    rtm._tl = rtm.timelines.current = rtm.timelines.create().timeline
-    return rtm
+        self._tl = self.timelines.current = self.timelines.create().timeline
+
+        #rtm.LISTS = getListDict()
+        self.LISTS = {u'Daily': u'4574767', u'Inbox': u'3917063', u'Sent': u'3917067'}
+
 
 def addTask(rtm, name, tags=None,
         due_date=None, estimate=None, repeat=None,
@@ -52,14 +65,14 @@ def addTask(rtm, name, tags=None,
 
     if atomic:
         rtm._tl = rtm.timelines.create().timeline
-    
+
     try:
         tr = rtm.tasks.add(timeline=rtm._tl,name=name,parse=parse)
         list_id = tr.list.id
         tser_id = tr.list.taskseries.id
         task_id = tr.list.taskseries.task.id
         logging.debug(':'.join([list_id, tser_id, task_id]))
-        
+
         if tags:
             rtm.tasks.addTags(timeline=rtm._tl, list_id=list_id,
                     taskseries_id=tser_id, task_id=task_id,
@@ -80,25 +93,40 @@ def addTask(rtm, name, tags=None,
             rtm.tasks.setPriority(timeline=rtm._tl, list_id=list_id,
                     taskseries_id=tser_id, task_id=task_id,
                     priority=priority)
+
+        if settags:
+            rtm.tasks.setTags(timeline=rtm._tl, list_id=list_id,
+                    taskseries_id=tser_id, task_id=task_id,
+                    tags=','.join(settags))
+
     except:
         logging.error("Problem with adding a task")
         # TODO: Handle transactions correctly
 
     return (list_id, task_id, tser_id)
 
-    
-def test_addTask(rtm=rtm_connection()):
+
+def test_addTask(rtm=RMilk()):
     addTask(rtm,"wow",tags=['pretty','cool'],parse=False)
 
 
-def getTimezones(rtm=rtm_connection()):
+def getTimezones(rtm=RMilk()):
     print '\n'.join([' : '.join(
         [x.name, x.id, x.dst, x.offset, x.current_offset])
         for x in tz.timezones.timezone if 'America' in x.name])
 
+def getListDict(rtm=RMilk()):
+    r = rtm.lists.getList()
+    return dict(map(lambda x:(x.name, x.id), filter(lambda x: x.smart == u'0', r.lists.list)))
+
+def getList(rtm=RMilk(), list_id=None, filter=''):
+    rtm.tasks.getList(list_id, filter=filter)
+
+    # cast return structure to task
+
 TEMPLATE_TASK = \
-Template(
-'''Name: ${name}
+        Template(
+                '''Name: ${name}
 Tags: ${tags}
 Due : ${due_date}
 Est : ${estimate}
@@ -106,8 +134,8 @@ Pri : ${priority}''')
 
 class Task(object):
     def __init__(self, name, tags=None, due_date=None, estimate=None,
-                        repeat=None, priority=None, parse=None,
-                        id=None, list_id=None):
+            repeat=None, priority=None, parse=None,
+            id=None, list_id=None):
         self.name=name
         self.tags=tags
         self.due_date=due_date
@@ -123,10 +151,10 @@ class Task(object):
         try:
             (self.list_id, self.id, self.tser_id) = \
                     addTask(rtm, self.name, self.tags, self.due_date,
-                    self.estimate,self.repeat, self.priority, True, True)
-            # update self with relevant
+                            self.estimate,self.repeat, self.priority, True, True)
+                    # update self with relevant
             logging.info("Saved task: %s (%s:%s)" % \
-                    (self.name, self.list_id, self.id)
+                    (self.name, self.list_id, self.id))
             return True
         except:
             logging.error("Error saving task")
@@ -141,14 +169,14 @@ class Task(object):
             'estimate': self.estimate or '',
             'priority': self.priority or ''})
 
-    def __str_detailed__(self):
-        print str(self)
-        print 'id  :', self.id
-        print 'lid :', self.list_id
-        print 'tser:', self.tser_id
- 
+        def __str_detailed__(self):
+            print str(self)
+            print 'id  :', self.id
+            print 'lid :', self.list_id
+            print 'tser:', self.tser_id
 
-def test_Task(rtm=rtm_connection()):
+
+def test_Task(rtm=RMilk()):
     t = Task("wow",tags=['prety','@cool'], due_date="tomorrow", estimate="10 minutes")
     t.save(rtm)
     print t.__str_detailed__()
@@ -157,7 +185,7 @@ def test_Task(rtm=rtm_connection()):
 
 
 if __name__=="__main__":
-#    print '\n'.join(map(str,examp(rtm_connection())))
+    #    print '\n'.join(map(str,examp(RMilk())))
     from optparse import OptionParser
     from operator import xor
     arg = OptionParser(version="%prog 0.1")
@@ -172,6 +200,7 @@ if __name__=="__main__":
     arg.add_option('-p','--priority',dest='priority',help='Task priority')
     arg.add_option('-l','--list',dest='get_list',help='Get list by filter')
 
+    arg.add_options('--rename',dest='tag_rename',nargs=2,help='Rename <original> with <tag>. Requires list filter')
     arg.add_option('--test',dest='test',action='store_true',help='Testing')
 
     (options, args) = arg.parse_args()
@@ -186,19 +215,31 @@ if __name__=="__main__":
         exit()
 
     if options.test:
-        rtm = rtm_connection()
+        rtm = RMilk()
         test_Task(rtm)
         exit()
- 
+
     # Normalize tags list
     if options.tags:
         options.tags = map(str.strip, options.tags.split(','))
-    
+
     if options.create:
         t = Task(options.name, options.tags, options.due_date,
                 options.estimate, options.repeat, options.priority,
                 parse=True)
-        t.save(rtm_connection())
+        t.save(RMilk())
 
     if options.get_list:
         print "list by formatter selection"
+
+    if options.tag_rename: # 2 options
+        # get list by filter
+        #for task in casted_to_task_tasklist:
+            #settags(current_tags - original + newtag
+            #print task
+        (old,new) = options.tag_rename
+        print "%s --> %s" % (old,new)
+
+
+#[(x.name, x.tags.tag or []) for x in r.tasks.getList(filter="tagContains:.is").tasks.list[1].taskseries]
+
